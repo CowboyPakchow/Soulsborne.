3f41f3cc3fa1cc35f65e196a104f316abf7f5599
Michael Nguyen <michael.nguyen@rbe.sk.ca>
2020-10-26 22:37:54 +0000

    Updated main with new actions to perform in Soulsborne. Game also has an ending now.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # Description: Main file to run Soulsborne.
 
 from player import Player
-import floor1
+import floor
 from collections import OrderedDict
 from time import sleep
 import sys
@@ -41,22 +41,24 @@ def add_action(action_dict, hotkey, action, name):
 
 def move_player(actions, player, position):
     """Depending on the player's position, movement is changed."""
-    if floor1.tile_at(position.x, position.y - 1):
+    if floor.tile_at(position.x, position.y - 1):
         return add_action(actions, "North", player.move_north, "Move North.")
-    if floor1.tile_at(position.x, position.y + 1):
+    if floor.tile_at(position.x, position.y + 1):
         return add_action(actions, "South", player.move_south, "Move South.")
-    if floor1.tile_at(position.x - 1, position.y):
+    if floor.tile_at(position.x - 1, position.y):
         return add_action(actions, "West", player.move_west, "Move West.")
-    if floor1.tile_at(position.x + 1, position.y):
+    if floor.tile_at(position.x + 1, position.y):
         return add_action(actions, "East", player.move_east, "Move East")
 
     def quit(self):
         """Quits out of game"""
         while True:
-            word = input("Are you sure you want to quit? ('yes to quit.')")
+            word = input("Are you sure you want to quit? ")
             if word in ['yes']:
                 print("They always return eventually...")
                 sys.exit()
+            else:
+                break
 
 
 def actions_available(position, player):
@@ -68,31 +70,86 @@ def actions_available(position, player):
     # Prints items inside the inventory (If there are any.)
     if player.inventory:
         add_action(actions, "Inventory", player.print_inventory, "Inventory")
-    # Prints the map of the first floor.
-    if isinstance(position, floor1.ViewMapTile):
-        add_action(actions, "Map", position.print_map, "Floor 1 Map")
+    # Prints the map of the Starting Room.
+    if isinstance(position, floor.ViewMapTile):
+        add_action(actions, "Map", position.print_map, "Floor Map")
+    if isinstance(position,floor.RightMap):
+        add_action(actions, "Map", position.print_map, "Right Path Map")
+    if isinstance(position, floor.LeftMap):
+        add_action(actions, "Map", position.print_map, "Left Path Map")
+    if isinstance(position, floor.TopMap):
+        add_action(actions, "Map", position.print_map, "Top Path Map")
+    if isinstance(position, floor.BotMap):
+        add_action(actions, "Map", position.print_map, "Bot Path Map")
     # Add an option for items if there are any items remaining.
-    if isinstance(position, floor1.ItemTile) and position.inventory:
+    if isinstance(position, floor.ItemTileL) and position.inventory:
+        add_action(actions, "Add", player.add_items, "Add Items")
+    if isinstance(position, floor.ItemTileR) and position.inventory:
+        add_action(actions, "Add", player.add_items, "Add Items")
+    if isinstance(position, floor.ItemTileT) and position.inventory:
+        add_action(actions, "Add", player.add_items, "Add Items")
+    if isinstance(position, floor.ItemTileB) and position.inventory:
+        add_action(actions, "Add", player.add_items, "Add Items")
+    if isinstance(position, floor.BossLootL) and position.inventory:
+        add_action(actions, "Add", player.add_items, "Add Items")
+    if isinstance(position, floor.BossLootR) and position.inventory:
+        add_action(actions, "Add", player.add_items, "Add Items")
+    if isinstance(position, floor.BossLootT) and position.inventory:
+        add_action(actions, "Add", player.add_items, "Add Items")
+    if isinstance(position, floor.SpecialLootT) and position.inventory:
+        add_action(actions, "Add", player.add_items, "Add Items")
+    if isinstance(position, floor.SpecialLootR) and position.inventory:
+        add_action(actions, "Add", player.add_items, "Add Items")
+    if isinstance(position, floor.SpecialLootL) and position.inventory:
+        add_action(actions, "Add", player.add_items, "Add Items")
+    if isinstance(position, floor.ArtoriasLoot) and position.inventory:
         add_action(actions, "Add", player.add_items, "Add Items")
     # Attack and Defend options when in battle with enemy.
-    elif isinstance(position, floor1.EnemyTile) and position.enemy.is_alive():
+    elif isinstance(position, floor.EnemyTileR) and position.enemy.is_alive():
         add_action(actions, "Attack", player.attack, "Attack")
         add_action(actions, "Defend", player.defend, "Defend")
-    elif isinstance(position, floor1.OptionalTile) and position.enemy.is_alive():
+    elif isinstance(position, floor.EnemyTileT) and position.enemy.is_alive():
+        add_action(actions, "Attack", player.attack, "Attack")
+        add_action(actions, "Defend", player.defend, "Defend")
+    elif isinstance(position, floor.EnemyTileL) and position.enemy.is_alive():
+        add_action(actions, "Attack", player.attack, "Attack")
+        add_action(actions, "Defend", player.defend, "Defend")
+    elif isinstance(position, floor.EnemyTileB) and position.enemy.is_alive():
+        add_action(actions, "Attack", player.attack, "Attack")
+        add_action(actions, "Defend", player.defend, "Defend")
+    elif isinstance(position, floor.OptionalTileL) and position.enemy.is_alive():
+        add_action(actions, "Attack", player.attack, "Attack")
+        add_action(actions, "Defend", player.defend, "Defend")
+    elif isinstance(position, floor.OptionalTileR) and position.enemy.is_alive():
+        add_action(actions, "Attack", player.attack, "Attack")
+        add_action(actions, "Defend", player.defend, "Defend")
+    elif isinstance(position, floor.OptionalTileT) and position.enemy.is_alive():
+        add_action(actions, "Attack", player.attack, "Attack")
+        add_action(actions, "Defend", player.defend, "Defend")
+    elif isinstance(position, floor.BossTileR) and position.enemy.is_alive():
+        add_action(actions, "Attack", player.attack, "Attack")
+        add_action(actions, "Defend", player.defend, "Defend")
+    elif isinstance(position, floor.BossTileL) and position.enemy.is_alive():
+        add_action(actions, "Attack", player.attack, "Attack")
+        add_action(actions, "Defend", player.defend, "Defend")
+    elif isinstance(position, floor.BossTileT) and position.enemy.is_alive():
+        add_action(actions, "Attack", player.attack, "Attack")
+        add_action(actions, "Defend", player.defend, "Defend")
+    elif isinstance(position, floor.BossTileB) and position.enemy.is_alive():
         add_action(actions, "Attack", player.attack, "Attack")
         add_action(actions, "Defend", player.defend, "Defend")
     # Move to another tile when all other actions are completed.
     else:
-        if floor1.tile_at(position.x, position.y - 1):
+        if floor.tile_at(position.x, position.y - 1):
             add_action(actions, "North", player.move_north, "Move North.")
-        if floor1.tile_at(position.x, position.y + 1):
+        if floor.tile_at(position.x, position.y + 1):
             add_action(actions, "South", player.move_south, "Move South.")
-        if floor1.tile_at(position.x - 1, position.y):
+        if floor.tile_at(position.x - 1, position.y):
             add_action(actions, "West", player.move_west, "Move West.")
-        if floor1.tile_at(position.x + 1, position.y):
+        if floor.tile_at(position.x + 1, position.y):
             add_action(actions, "East", player.move_east, "Move East.")
-    # Healing for when the player's hp value is lower than 40.
-    if player.hp < 40:
+    # Healing for when the player's hp value is lower than 60.
+    if player.hp < 60:
         add_action(actions, "Heal", player.heal, "Heal")
     if player.quit:
         add_action(actions, "Quit", player.quit, "Quit out of game.")
@@ -119,12 +176,12 @@ def play():
     """ Character movement and accessing the inventory."""
     print("Welcome to...")
     intro_text()
-    floor1.parse_floor1_dsl()
+    floor.parse_floor_dsl()
     player = Player()
     # Player directions and actions they can perform while alive.
     while player.is_alive() and not player.victory:
         # Defining the player's starting position
-        position = floor1.tile_at(player.x, player.y)
+        position = floor.tile_at(player.x, player.y)
         # The intro of the game is printed at the starting position.
         print(position.intro_text())
         # Health points of the player are modified when they take damage.
