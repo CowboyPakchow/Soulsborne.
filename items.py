2ced98803af8a97ac6b7f69d14e46237ae48c3e5
Michael Nguyen <michael.nguyen@rbe.sk.ca>
2020-10-26 22:31:37 +0000

    Modified Items by changing some Damage and Armour values to make it more forgiving for the player.

--- items.py
+++ items.py
@@ -1,7 +1,7 @@
 # Course: CS 30
 # Period: 1
 # Date Created: 20/10/14
-# Date Modified: 20/10/16
+# Date Modified: 20/10/26
 # Name: Michael Nguyen
 # Description: Items class for Soulsborne.
 
@@ -14,7 +14,7 @@ class Weapon:
         return "{} (+ {} Damage)".format(self.name, self.damage)
 
 
-# Floor 1 Weapons
+# Right Path Weapons.
 class Rapier(Weapon):
     def __init__(self):
         self.name = "Rapier"
@@ -26,24 +26,24 @@ class Broadsword(Weapon):
     def __init__(self):
         self.name = "Broadsword"
         self.description = "Slightly better than the Rapier?..."
-        self.damage = 12
+        self.damage = 15
 
 
 class Greatsword(Weapon):
     def __init__(self):
         self.name = "Greatsword"
         self.description = "A Large Sword, quite powerful in fact."
-        self.damage = 15
+        self.damage = 20
 
 
 class Hollow_Blade(Weapon):
     def __init__(self):
         self.name = "Hollow Blade"
         self.description = "A sword given to all the Knights of Arturia."
-        self.damage = 20
+        self.damage = 25
 
 
-# Floor 2 weapons
+# Left Path Weapons.
 class Zombie_Arm(Weapon):
     def __init__(self):
         self.name = "Zombie Arm"
@@ -55,22 +55,30 @@ class Secace(Weapon):
     def __init__(self):
         self.name = "Secace"
         self.description = "A mad knight's sword from a distant land..."
-        self.damage = 27
+        self.damage = 35
 
 
 class Excalibrate(Weapon):
     def __init__(self):
         self.name = "Excalibrate"
         self.description = "From the challenges of The Dungeonmaster..."
-        self.damage = 30
+        self.damage = 45
 
 
-# Secret Floor Weapon
+# Top Path Weapons
+class Cinderhulk(Weapon):
+    def __init__(self):
+        self.name = "Cinderhulk"
+        self.description = "The Flame has never been extinguished."
+        self.damage = 65
+
+
+# Bottom Path Weapons.
 class Abysswalker(Weapon):
     def __init__(self):
         self.name = "Abysswalker"
         self.description = "From beyond and back."
-        self.damage = 45
+        self.damage = 80
 
 
 class Armour:
@@ -80,39 +88,50 @@ class Armour:
     def __str__(self):
         return "{} (- {} Damage)".format(self.name, self.protection)
 
-
+# Right Path Armour.
 class Basic_Shield(Armour):
     def __init__(self):
         self.name = "Basic Shield"
         self.description = "A basic shield for blocking, nothing special."
-        self.protection = 10
+        self.protection = 15
 
 
 class Hollow_Shield(Armour):
     def __init__(self):
         self.name = "Hollow Shield"
         self.description = "A shield given out to the knights of Arturia."
-        self.protection = 15
+        self.protection = 25
 
 
-class Skeleton_Skull(Armour):
+class Slime_Barrier(Armour):
     def __init__(self):
-        self.name = "Skeleton Skull"
-        self.description = "You really like picking up dead things huh."
-        self.protection = 1
+        self.name = "Slime Barrier"
+        self.description = "A Protective Coating."
+        self.protection = 35
 
 
+# Left Path Armour.
 class Galahad_Shield(Armour):
     def __init__(self):
         self.name = "Galahad's Shield"
         self.description = "A pure shield."
-        self.protection = 30
+        self.protection = 50
 
 
+# Top Path Armour.
+class Guardians_Safeguard(Armour):
+    def __init__(self):
+        self.name = "Guardian's Safeguard"
+        self.description = "The Guardian's Will"
+        self.protection = 70
+
+
+# Bottom Path Armour.
 class Abysswalker_Arm(Armour):
     def __init__(self):
         self.name = "Abysswalker's Arm"
-        self.description = "The Left Arm Armour of Artorias, "
+        self.description = "The Left Arm Armour of Artorias, the ancient."
+        self.protection = 90
 
 
 class Consumable:
@@ -141,7 +160,7 @@ class Estus(Consumable):
     def __init__(self):
         self.name = "Estus Flask"
         self.description = "Healing potion...seems familiar..."
-        self.healing = 20
+        self.healing = 25
 
 
 class Humanity(Consumable):
@@ -151,13 +170,20 @@ class Humanity(Consumable):
         self.healing = 99
 
 
-class Currency:
+class Misc:
     def __init__(self):
-        raise NotImplementedError("Do no create raw Currency objects/")
+        raise NotImplementedError("Do no create raw Misc objects.")
 
+    def __str__(self):
+        return "{} ({})".format(self.name, self.description)
 
-class Gold_Coins(Currency):
+class Gold_Coins(Misc):
     def __init__(self):
         self.name = "Gold Coins"
         self.description = "Coins for bartering"
 
+class Champions_Chalice(Misc):
+    def __init__(self):
+        self.name = "Champions Chalice"
+        self.description = "The end is the beginning, take this back to start."
+        
