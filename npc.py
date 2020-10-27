9e9c05f1bc13803ec4ca54eae587215fe290dbac
Michael Nguyen <michael.nguyen@rbe.sk.ca>
2020-10-27 00:02:08 +0000

    Added NPC that players can trade with. Merchant has items in inventory.

---
+++ npc.py
@@ -0,0 +1,27 @@
+# Course: CS 30
+# Period: 1
+# Date Created: 20/10/14
+# Date Modified: 20/10/26
+# Name: Michael Nguyen
+# Description: NPC class for Soulsborne.
+
+import items
+
+class NonPlayableCharacter():
+    def __init__(self):
+        raise NotImplementedError("Do not create raw NPC objects.")
+
+    def __str__(self):
+        return self.Name
+
+
+class Trader(NonPlayableCharacter):
+    def __init__(self):
+        self.name = "Merchant"
+        self.gold = 1000
+        self.inventory = [items.Estus(), items.Humanity(), items.Greatsword(),
+                          items.Broadsword(), items.Great_Potion(), items.Potion(),
+                          items.Potion(), items.Great_Potion(), 
+                          items.Great_Potion(), items.Great_Potion(), 
+                          items.Great_Potion(), items.Potion(),
+                          items.Potion(), items.Great_Potion(), items.Potion(),]
